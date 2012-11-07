2a493d23d5d7fe8d2d08262ac526954a8eb89805
Iikka Eklund <iikka.eklund@digia.com>
2012-11-07 14:51:37 +0200

    Fix regular expression in icu path patcher
    Fixed a regular expression that matches the icu_install paths in
    non-binary files. Caused broken files.
    
    Change-Id: Ia988fe08a29f0fb7d23a236929a23e12422d86a3
    Reviewed-by: Sergio Ahumada <sergio.ahumada@digia.com>
    Reviewed-by: Johanna Äijälä <johanna.aijala@digia.com>

--- release-tools/configurations/pkg/qt.500_beta2.gcc_64.essentials/data/patcher.sh
+++ release-tools/configurations/pkg/qt.500_beta2.gcc_64.essentials/data/patcher.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/sh
+#!/usr/bin/env bash
 
 #echo "Patcher arguments: $1"
 if [ $# -lt 1 ]; then
@@ -13,5 +13,5 @@ done < <(grep -riIl -e 'icu_install' $1)
 #echo "Grepping matches..done"
 
 #echo "Sedding.."
-sed -i -e 's/-[I|L]\"*\/data\/bamboo\/xml-data\/build-dir.*icu_install\/[lib|include]\"*.//g' "${tmpfilenames[@]}"
+sed -i -e 's/-[I|L]\"*\/data\/bamboo\/xml-data\/build-dir.*icu_install\/.*[\" $]//g' "${tmpfilenames[@]}"
 
