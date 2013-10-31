7f24316432523f41e1ed12857b29db01572f779f
Eike Ziller <eike.ziller@digia.com>
2013-10-31 16:11:21 +0100

    Qt Creator Enterprise: Build b2qt plugin
    Change-Id: I9b9d35c40e94b059c7459b6e01431589db355e91
    Reviewed-by: Kai Koehne <kai.koehne@digia.com>

--- packaging-tools/build_wrapper.py
+++ packaging-tools/build_wrapper.py
@@ -940,6 +940,8 @@ def handle_qt_creator_build():
     if LICENSE == 'enterprise':
         cmd_args.extend(['--additional_plugin', os.path.normpath(WORK_DIR + '/qmlprofiler'),
                          '--additional_plugin', os.path.normpath(WORK_DIR + '/qtquickdesigner')])
+        if bldinstallercommon.is_linux_platform():
+            cmd_args.extend(['--additional_plugin', os.path.normpath(WORK_DIR + '/b2qt-qtcreator-plugin')])
 
     if bldinstallercommon.is_linux_platform():
         cmd_args.extend(['--icu7z', ICU_LIBS])
