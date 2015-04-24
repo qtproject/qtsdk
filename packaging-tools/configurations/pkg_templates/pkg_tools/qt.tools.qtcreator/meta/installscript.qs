qtproject/qtsdk

commit cc8c5045c50e44464138f92b56a16b381d7b8129

Creator: Fix check for running instance
The old logic to get the executable path was broken. Use the path
calculated in by reactOnTargetDirChange instead.

Also remove check for linguist.exe & qmlviewer.exe (which aren't
shiped anymore since ages), and change the working directory of
Qt Creator to the user's home directory, as the standalone installer
does.

Change-Id: Ifcd26894a80556c3ad70f97a215359a05d7929b1
Reviewed-by: Iikka Eklund <iikka.eklund@theqtcompany.com>
Reviewed-by: Eike Ziller <eike.ziller@theqtcompany.com>

packaging-tools/configurations/pkg_templates/pkg_tools/qt.tools.qtcreator/meta/installscript.qs
@@ -39,7 +39,6 @@
 **
 ****************************************************************************/
 
-var component_root_path = "/";
 var native_path_separator = "/";
 
 // constructor
@@ -77,12 +76,8 @@ Component.prototype.loaded = function()
 
 Component.prototype.beginInstallation = function()
 {
-    if (installer.value("os") === "win") {
-        var path = component_root_path + native_path_separator + "bin" + native_path_separator;
-        component.setStopProcessForUpdateRequest(path + "qtcreator.exe", true);
-        component.setStopProcessForUpdateRequest(path + "linguist.exe", true);
-        component.setStopProcessForUpdateRequest(path + "qmlviewer.exe", true);
-    }
+    if (installer.value("os") === "win")
+        component.addStopProcessForUpdateRequest(component.qtCreatorBinaryPath);
 }
 
 Component.prototype.reactOnTargetDirChange = function(key, value)
@@ -108,20 +103,6 @@ Component.prototype.reactOnTargetDirChange = function(key, value)
             component.qtCreatorBinaryPath = component.qtCreatorBinaryPath.replace(/\/+/g, "/");
         }
     }
-}
-
-function buildNativeComponentRootPath()
-{
-    var target_install_dir = "%TARGET_INSTALL_DIR%";
-    if (installer.value("os") == "win") {
-        native_path_separator = "\\";
-        target_install_dir = target_install_dir.replace(/\//g, "\\");
-    }
-    else {
-        native_path_separator = "/";
-    }
-
-    component_root_path = installer.value("TargetDir") + target_install_dir;
 }
 
 registerCommonWindowsFileTypeExtensions = function()
@@ -209,15 +190,13 @@ Component.prototype.createOperations = function()
 {
     // Call the base createOperations and afterwards set some registry settings
     component.createOperations();
-    buildNativeComponentRootPath();
-    var path = component_root_path + native_path_separator;
     var maintenanceToolPath;
 
     if (installer.value("os") == "win") {
         component.addOperation("CreateShortcut",
                                component.qtCreatorBinaryPath,
                                "@StartMenuDir@\\Qt Creator (Community).lnk",
-                               "workingDirectory=" + path + "bin");
+                               "workingDirectory=@homeDir@");
 
         registerWindowsFileTypeExtensions();
 

commit e1dde44db1790f7d06c766559dfd6793b2904ce9

QtCreator: Fix 'ReferenceError: path is not defined'
Fix regression introduced in cc8c5045c50e44464 .

Task-number: QTIFW-680
Change-Id: I26b31a9364d29b3fc2807753841c75118def13ab
Reviewed-by: Eike Ziller <eike.ziller@theqtcompany.com>
Reviewed-by: Iikka Eklund <iikka.eklund@theqtcompany.com>

packaging-tools/configurations/pkg_templates/pkg_tools/qt.tools.qtcreator/meta/installscript.qs
@@ -206,7 +206,7 @@ Component.prototype.createOperations = function()
         maintenanceToolPath = installer.value("TargetDir") + "/MaintenanceTool.exe";
     }
     if (installer.value("os") == "x11") {
-        component.addOperation("InstallIcons", path + "share" + native_path_separator + "icons" );
+        component.addOperation("InstallIcons", installer.value("TargetDir") + "%TARGET_INSTALL_DIR%/share/icons" );
         component.addOperation("CreateDesktopEntry",
                                "DigiaQt-qtcreator-community.desktop",
                                "Type=Application\nExec=" + component.qtCreatorBinaryPath + "\nName=Qt Creator (Community)\nGenericName=The IDE of choice for Qt development.\nIcon=QtProject-qtcreator\nTerminal=false\nCategories=Development;IDE;Qt;\nMimeType=text/x-c++src;text/x-c++hdr;text/x-xsrc;application/x-designer;application/vnd.qt.qmakeprofile;application/vnd.qt.xml.resource;text/x-qml;text/x-qt.qml;text/x-qt.qbs;");