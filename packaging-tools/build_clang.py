59167f774a25858e28714b422dba77378324dabb
Nikolai Kosjar <nikolai.kosjar@qt.io>
2016-09-02 15:58:53 +0200

    build_clang.py: Build also with mingw toolchain
    build_clnag() now uses bldinstallercommon.do_execute_sub_process() instead of
    bld_utils.runCommand(). The latter merges the given environment with the
    process environment, which is unfavorable:
    
     1. We need git.exe in the PATH.
     2. git.exe comes together with sh.exe.
     3. but cmake/mingw must not find sh.exe in the PATH.
    
    Change-Id: I57ce3cfe0f0792e9badf765fd63332ca5f74c383
    Reviewed-by: Eike Ziller <eike.ziller@qt.io>

--- packaging-tools/build_clang.py
+++ packaging-tools/build_clang.py
@@ -63,8 +63,17 @@ def apply_patches(src_path, patch_filepaths):
     for patch in patch_filepaths:
         apply_patch(src_path, patch)
 
-def cmake_generator():
-    return 'NMake Makefiles JOM' if bldinstallercommon.is_win_platform() else 'Unix Makefiles'
+def is_msvc_toolchain(toolchain):
+    return 'msvc' in toolchain
+
+def is_mingw_toolchain(toolchain):
+    return 'mingw' in toolchain
+
+def cmake_generator(toolchain):
+    if bldinstallercommon.is_win_platform():
+        return 'MinGW Makefiles' if is_mingw_toolchain(toolchain) else 'NMake Makefiles JOM'
+    else:
+        return 'Unix Makefiles'
 
 def bitness_flags(bitness):
     if bitness == 64:
@@ -75,23 +84,33 @@ def bitness_flags(bitness):
             flags.extend(['-DLIBXML2_LIBRARIES=/usr/lib/libxml2.so', '-DLLVM_BUILD_32_BITS=ON'])
     return flags
 
-def make_command():
-    return ["jom"] if bldinstallercommon.is_win_platform() else ["make"]
+def make_command(toolchain):
+    if bldinstallercommon.is_win_platform():
+        return ['mingw32-make'] if is_mingw_toolchain(toolchain) else ['jom']
+    else:
+        return ['make']
 
-def install_command():
-    return ["nmake", "install"] if bldinstallercommon.is_win_platform() else ["make", "-j1", "install"]
+def install_command(toolchain):
+    if bldinstallercommon.is_win_platform():
+        return ['mingw32-make', 'install'] if is_mingw_toolchain(toolchain) else ["nmake", "install"]
+    else:
+        return ["make", "-j1", "install"]
 
-def build_clang(src_path, build_path, install_path, bitness=64, environment=None, build_type='Release'):
+def build_clang(toolchain, src_path, build_path, install_path, bitness=64, environment=None, build_type='Release'):
+    if build_path and not os.path.lexists(build_path):
+        os.makedirs(build_path)
+
     cmake_command = ['cmake',
                      '-DCMAKE_INSTALL_PREFIX=' + install_path,
                      '-G',
-                     cmake_generator(),
+                     cmake_generator(toolchain),
                      '-DCMAKE_BUILD_TYPE=' + build_type]
     cmake_command.extend(bitness_flags(bitness))
     cmake_command.append(src_path)
-    bld_utils.runCommand(cmake_command, build_path, init_environment=environment)
-    bld_utils.runCommand(make_command(), build_path, init_environment=environment)
-    bld_utils.runCommand(install_command(), build_path, init_environment=environment)
+
+    bldinstallercommon.do_execute_sub_process(cmake_command, build_path, extra_env=environment)
+    bldinstallercommon.do_execute_sub_process(make_command(toolchain), build_path, extra_env=environment)
+    bldinstallercommon.do_execute_sub_process(install_command(toolchain), build_path, extra_env=environment)
 
 def package_clang(install_path, result_file_path):
     (basepath, dirname) = os.path.split(install_path)
@@ -104,14 +123,25 @@ def upload_clang(file_path, remote_path):
     scp_command = [scp_bin, filename, remote_path]
     bld_utils.runCommand(scp_command, path)
 
-def build_environment(bitness):
+def paths_with_sh_exe_removed(path_value):
+    items = path_value.split(os.pathsep)
+    items = [i for i in items if not os.path.exists(os.path.join(i, 'sh.exe'))]
+    return os.pathsep.join(items)
+
+def build_environment(toolchain, bitness):
     if bldinstallercommon.is_win_platform():
-        program_files = os.path.join('C:', '/Program Files (x86)')
-        if not os.path.exists(program_files):
-            program_files = os.path.join('C:', '/Program Files')
-        vcvarsall = os.path.join(program_files, 'Microsoft Visual Studio ' + os.environ['MSVC_VERSION'], 'VC', 'vcvarsall.bat')
-        arg = 'amd64' if bitness == 64 else 'x86'
-        return environmentfrombatchfile.get(vcvarsall, arguments=arg)
+        if is_mingw_toolchain(toolchain):
+            environment = dict(os.environ)
+            # cmake says "For MinGW make to work correctly sh.exe must NOT be in your path."
+            environment['PATH'] = paths_with_sh_exe_removed(environment['PATH'])
+            return environment
+        else:
+            program_files = os.path.join('C:', '/Program Files (x86)')
+            if not os.path.exists(program_files):
+                program_files = os.path.join('C:', '/Program Files')
+            vcvarsall = os.path.join(program_files, 'Microsoft Visual Studio ' + os.environ['MSVC_VERSION'], 'VC', 'vcvarsall.bat')
+            arg = 'amd64' if bitness == 64 else 'x86'
+            return environmentfrombatchfile.get(vcvarsall, arguments=arg)
     else:
         return None # == process environment
 
@@ -125,13 +155,14 @@ def main():
     build_path = os.path.join(base_path, 'build')
     install_path = os.path.join(base_path, 'libclang')
     bitness = 64 if '64' in os.environ['cfg'] else 32
-    environment = build_environment(bitness)
+    toolchain = os.environ['cfg'].split('-')[1].lower()
+    environment = build_environment(toolchain, bitness)
     result_file_path = os.path.join(base_path, 'libclang-' + branch + '-' + os.environ['CLANG_PLATFORM'] + '.7z')
     remote_path = (os.environ['PACKAGE_STORAGE_SERVER_USER'] + '@' + os.environ['PACKAGE_STORAGE_SERVER'] + ':'
                    + os.environ['PACKAGE_STORAGE_SERVER_BASE_DIR'] + '/' + os.environ['CLANG_UPLOAD_SERVER_PATH'])
     get_clang(base_path, os.environ['LLVM_REVISION'], os.environ['CLANG_REVISION'])
     apply_patches(clang_src_path, sorted(glob.glob(os.path.join(patch_src_path, '*'))))
-    build_clang(src_path, build_path, install_path, bitness, environment, build_type='Release')
+    build_clang(toolchain, src_path, build_path, install_path, bitness, environment, build_type='Release')
     package_clang(install_path, result_file_path)
     upload_clang(result_file_path, remote_path)
 
