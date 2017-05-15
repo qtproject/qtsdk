1f0b9e9a6305ea40fe323d53e650b35746785bd8
Iikka Eklund <iikka.eklund@qt.io>
2017-05-15 14:09:54 +0300

    Check also 'tar.xz' extension for IFW builds
    Change-Id: Iecf284ac37434a89a04b4f8d2046d966db902f95
    Reviewed-by: Katja Marttila <katja.marttila@qt.io>
    Reviewed-by: Antti Kokko <antti.kokko@qt.io>

--- packaging-tools/build_wrapper.py
+++ packaging-tools/build_wrapper.py
@@ -170,7 +170,7 @@ def handle_ifw_build(optionDict):
     os.chdir(SCRIPT_ROOT_DIR)
     # Qt
     qt_src_pkg = optionDict['IFW_QT_SRC_PKG'] # mandatory env variable
-    if not qt_src_pkg.endswith(('.zip', '.7z', '.tar.gz')):
+    if not qt_src_pkg.endswith(('.zip', '.7z', '.tar.gz', '.tar.xz')):
         qt_src_pkg = qt_src_pkg + '.zip' if bldinstallercommon.is_win_platform() else qt_src_pkg + '.tar.gz'
     # OpenSSL
     openssl_dir = optionDict.get('IFW_OPENSSL_DIR', '')
