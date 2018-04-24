d596f4015f90eb0cd74a78c3ac504a8610e4d94e
Eike Ziller <eike.ziller@qt.io>
2018-04-24 09:25:22 +0200

    Qt Creator: Ship Qt Wayland platform plugin
    Task-number: QTCREATORBUG-18914
    Change-Id: I31ea3efd4cee99130331fa31a051b6aa70c28137
    Reviewed-by: Simon Hausmann <simon.hausmann@qt.io>

--- packaging-tools/build_wrapper.py
+++ packaging-tools/build_wrapper.py
@@ -791,7 +791,7 @@ def handle_qt_creator_build(optionDict, qtCreatorPlugins):
     qt_modules = ['qtbase', 'qtdeclarative', 'qtgraphicaleffects',
                   'qtimageformats', 'qtlocation', 'qtmacextras',
                   'qtquickcontrols', 'qtquickcontrols2', 'qtscript', 'qtsvg', 'qttools',
-                  'qttranslations', 'qtx11extras', 'qtxmlpatterns']
+                  'qttranslations', 'qtwayland', 'qtx11extras', 'qtxmlpatterns']
     qt_module_urls = module_urls(qt_modules)
     if qt_extra_module_url:
         qt_module_urls.append(qt_extra_module_url)
