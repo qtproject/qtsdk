5510bf5db2fb0d3c15bc6cb6174a842cc0c141f6
Cristian Adam <cristian.adam@gmail.com>
2019-06-20 11:46:05 +0200

    build_clang.py: Strip the build artefacts
    Change-Id: I43730153f0df4e183fd55d4f93c06e0a1b4415a2
    Reviewed-by: Nikolai Kosjar <nikolai.kosjar@qt.io>

--- packaging-tools/build_clang.py
+++ packaging-tools/build_clang.py
@@ -309,7 +309,7 @@ def build_clang(toolchain, src_path, build_path, install_path, profile_data_path
     cmake_cmd = cmake_command(toolchain, src_path, build_path, install_path, profile_data_path, first_run, bitness, build_type)
 
     bldinstallercommon.do_execute_sub_process(cmake_cmd, build_path, extra_env=environment)
-    build_and_install(toolchain, build_path, environment, ['libclang', 'clang', 'llvm-config'], ['install'])
+    build_and_install(toolchain, build_path, environment, ['libclang', 'clang', 'llvm-config'], ['install/strip'])
 
 def check_clang(toolchain, build_path, environment):
     if is_msvc_toolchain(toolchain) or is_mingw_toolchain(toolchain):
