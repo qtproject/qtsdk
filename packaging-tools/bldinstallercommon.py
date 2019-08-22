b539046a7a71f3a5f2f7bbb8cfd21ce2abfdf2ab
Iikka Eklund <iikka.eklund@qt.io>
2019-08-22 09:29:44 +0000

    Revert "bldinstallercommon.py: use shutil.rmtree instead of native call"
    This reverts commit 3ffacfb6ae46ed2e5eddac3192921d37aabe957d.
    
    Reason for revert: Causes regression on Windows
    
    Change-Id: I6b54dce12ddcad3be30406ef58cce6da32e47738
    Reviewed-by: Jani Heikkinen <jani.heikkinen@qt.io>

--- packaging-tools/bldinstallercommon.py
+++ packaging-tools/bldinstallercommon.py
@@ -365,7 +365,16 @@ def remove_tree(path):
     if os.path.isdir(path) and os.path.exists(path):
         if IS_WIN_PLATFORM:
             path = win32api.GetShortPathName(path.replace('/', '\\'))
-        shutil.rmtree(path)
+            #a funny thing is that rmdir does not set an exitcode it is just using the last set one
+            try:
+                runCommand(['rmdir', path, '/S', '/Q'], SCRIPT_ROOT_DIR, onlyErrorCaseOutput=True)
+            except:
+                traceback.print_exc()
+                pass
+        else:
+            #shutil.rmtree(path)
+            runCommand(['rm', '-rf', path], SCRIPT_ROOT_DIR, onlyErrorCaseOutput=True)
+    return not os.path.exists(path)
 
 
 ###############################
