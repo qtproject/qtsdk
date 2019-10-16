026d9dd092fdfdf361bad2d5abbb9999af670a9e
Tim Jenssen <tim.jenssen@qt.io>
2019-10-16 21:29:14 +0200

    fix download from local file to not existing directory
    Change-Id: Id8772150ff10d304b658d4252adb73690ace3e0b
    Reviewed-by: Tim Jenssen <tim.jenssen@qt.io>

--- packaging-tools/bld_utils.py
+++ packaging-tools/bld_utils.py
@@ -133,6 +133,7 @@ def download(url, target, read_block_size = 1048576):
         # now a download can be a local path
         if os.path.lexists(url) and os.path.isfile(url):
             print("copying file from '{0}' to {1}".format(url, target))
+            os.makedirs(os.path.dirname(target))
             shutil.copy2(url, target)
             print("Done" + os.linesep)
             return
