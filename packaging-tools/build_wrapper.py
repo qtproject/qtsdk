qtproject/qtsdk

commit ab165a0e2ff6b9ad50e9c597ed1dca625a41e5ea

make the doc repack work around local URLs ready
Change-Id: Ie1ef3199722a397b495230d21a87ff648e73a993
Reviewed-by: Tim Jenssen <tim.jenssen@qt.io>

packaging-tools/build_wrapper.py
@@ -343,12 +343,18 @@ def move_files_to_parent_dir(source):
 
 
 def create_download_documentation_task(base_url, download_path):
-    urlpath = urlopen(base_url + "/doc")
-    string = urlpath.read().decode('utf-8')
-    pattern = re.compile('[0-9a-zA-Z-]*.zip')
+    doc_base_url = base_url + "/doc"
 
-    file_list = pattern.findall(string)
-    file_list = list(dict.fromkeys(file_list))
+    useLocal = urlparse.urlparse(doc_base_url).scheme != "file://"
+    if useLocal:
+        file_list = os.listdir(doc_base_url[len("file://"):])
+    else:
+        urlpath = urllib2.urlopen(doc_base_url)
+        string = urlpath.read().decode('utf-8')
+        pattern = re.compile('[0-9a-zA-Z-]*.zip')
+
+        file_list = pattern.findall(string)
+        file_list = list(dict.fromkeys(file_list))
 
     extract_path = os.path.join(download_path, 'tqtc-qt5-documentation')
     target_filepath = os.path.join(download_path, 'qt-everywhere-documentation.7z')

commit 44a4860e88df74e8708b62caca43138ac6608761

Fix documentation re-pack
Broke with ab165a0e2ff6b9ad50e9c597ed1dca625a41e5ea

Change-Id: I728000042e208c9a9629cd8f92900e20d1629f5d
Reviewed-by: Eike Ziller <eike.ziller@qt.io>

packaging-tools/build_wrapper.py
@@ -345,11 +345,11 @@ def move_files_to_parent_dir(source):
 def create_download_documentation_task(base_url, download_path):
     doc_base_url = base_url + "/doc"
 
-    useLocal = urlparse.urlparse(doc_base_url).scheme != "file://"
+    useLocal = urlparse.urlparse(doc_base_url).scheme == "file://"
     if useLocal:
         file_list = os.listdir(doc_base_url[len("file://"):])
     else:
-        urlpath = urllib2.urlopen(doc_base_url)
+        urlpath = urlopen(doc_base_url)
         string = urlpath.read().decode('utf-8')
         pattern = re.compile('[0-9a-zA-Z-]*.zip')
 