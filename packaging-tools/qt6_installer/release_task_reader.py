e2527a0e3f32f78e4a605b904c6411f245b6ccb1
Iikka Eklund <iikka.eklund@qt.io>
2020-02-07 12:46:41 +0200

    Add Release Task reader script
    A new version of the script to read and parse top level release
    configuration files.
    
    The parser uses extended variable interpolation i.e. .ini files may
    use more advanced syntax to declare and use variables.
    https://docs.python.org/3/library/configparser.html#configparser.ExtendedInterpolation
    
    The parser reads the .ini file with given filters which will match
    to section name attributes (comma separated). Each parsed task
    is presented by ReleaseTask class which can be further utilized.
    
    Tests included.
    
    Change-Id: I7682186ae5d61f57e32a2bc1e06c7855f9819cd7
    Reviewed-by: Antti Kokko <antti.kokko@qt.io>

---
+++ packaging-tools/qt6_installer/release_task_reader.py
@@ -0,0 +1,159 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#############################################################################
+##
+## Copyright (C) 2020 The Qt Company Ltd.
+## Contact: https://www.qt.io/licensing/
+##
+## This file is part of the release tools of the Qt Toolkit.
+##
+## $QT_BEGIN_LICENSE:GPL-EXCEPT$
+## Commercial License Usage
+## Licensees holding valid commercial Qt licenses may use this file in
+## accordance with the commercial license agreement provided with the
+## Software or, alternatively, in accordance with the terms contained in
+## a written agreement between you and The Qt Company. For licensing terms
+## and conditions see https://www.qt.io/terms-conditions. For further
+## information use the contact form at https://www.qt.io/contact-us.
+##
+## GNU General Public License Usage
+## Alternatively, this file may be used under the terms of the GNU
+## General Public License version 3 as published by the Free Software
+## Foundation with exceptions as appearing in the file LICENSE.GPL3-EXCEPT
+## included in the packaging of this file. Please review the following
+## information to ensure the GNU General Public License requirements will
+## be met: https://www.gnu.org/licenses/gpl-3.0.html.
+##
+## $QT_END_LICENSE$
+##
+#############################################################################
+
+import os
+import re
+import sys
+import argparse
+from typing import List, Dict
+from configparser import ConfigParser, ExtendedInterpolation
+from logging_util import init_logger
+
+log = init_logger(__name__, debug_mode=False)
+
+
+class ReleaseTaskError(Exception):
+    pass
+
+
+class ReleaseTask:
+
+    def __init__(self, name: str, settings: Dict[str, str]):
+        if not len(name.split(".")) >= 3:
+            raise ReleaseTaskError("The '[{0}]' has too few dot separated elements!".format(name))
+        self.name = name
+        self.config_file = settings["config_file"]
+        self.substitutions = settings.get("substitutions", "")
+        self.repo_path = settings.get("repo_path", "")
+        self.repo_components_to_update = settings.get("repo_components_to_update", "")
+        self.rta_key_list = settings.get("rta_key_list", "")
+        tmpList = [x.strip() for x in self.substitutions.split(',')] if self.substitutions else []  # type: List[str]
+        self.installer_string_replacement_list = list(filter(None, tmpList))
+        self.source_online_repository_path = ""
+        self.source_pkg_path = ""
+
+    def add_to_substitutions_list(self, substitutions: List[str]) -> None:
+        self.installer_string_replacement_list += substitutions
+
+    def is_repository_task(self) -> bool:
+        return self.name.split(".")[1] == "repository"
+
+    def is_offline_installer_task(self) -> bool:
+        return self.name.split(".")[1] == "offline"
+
+    def is_online_installer_task(self) -> bool:
+        return self.name.split(".")[1] == "online"
+
+    def get_config_file(self) -> str:
+        return self.config_file
+
+    def get_substitutions(self) -> str:
+        return self.substitutions
+
+    def get_installer_string_replacement_list(self) -> List[str]:
+        return self.installer_string_replacement_list
+
+    def get_repo_components_to_update(self) -> str:
+        return self.repo_components_to_update
+
+    def get_repo_path(self) -> str:
+        return self.repo_path
+
+    def get_rta_key_list(self) -> List[str]:
+        tmpList = self.rta_key_list.strip().replace(' ', '').split(",")
+        return list(filter(None, tmpList))
+
+    def get_source_online_repository_path(self) -> str:
+        # this points to local repository build path
+        return self.source_online_repository_path
+
+    def get_source_pkg_path(self) -> str:
+        # this points to local repository build path
+        return self.source_pkg_path
+
+
+def parse_substitutions_list(parser, section) -> List[str]:
+    try:
+        args = parser[section]['substitutions']
+        return [x.strip() for x in args.split(',')]
+    except KeyError:
+        # it's ok, the 'substitutions' is not mandatory
+        pass
+    return []
+
+
+def get_filter_parts(section_filters: str) -> List[str]:
+    return list(filter(None, re.split("[, ;:]+", section_filters)))
+
+
+def parse_data(settings, task_filters: List[str]) -> List[ReleaseTask]:
+    tasks = []  # type: List[ReleaseTask]
+    common_substitution_list = parse_substitutions_list(settings, 'common.substitutions')
+    section_filters_list = [get_filter_parts(x) for x in task_filters]
+
+    for section in settings.sections():
+        parts = section.split(".")
+        if not parts[0].startswith("task"):
+            continue
+        appendTask = True
+        if section_filters_list:
+            appendTask = False
+            for section_filters in section_filters_list:
+                if set(section_filters).issubset(set(parts)):
+                    appendTask = True
+                    break
+        if appendTask:
+            log.info("Parsing Task: %s", section)
+            releaseTask = ReleaseTask(section, settings[section])
+            releaseTask.add_to_substitutions_list(common_substitution_list)
+            tasks.append(releaseTask)
+        else:
+            log.info("Skipping task: [%s] - not included by task filter(s): %s", section, section_filters_list)
+    return tasks
+
+
+def parse_config(configFile: str, task_filters: List[str]) -> List[ReleaseTask]:
+    settings = ConfigParser(interpolation=ExtendedInterpolation())
+    settings.read(configFile)
+    return parse_data(settings, task_filters)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(prog="Script to parse top level release config file")
+    parser.add_argument("--config", dest="config", type=str, default=os.getenv("RELEASE_DESCRIPTION_FILE"),
+                        help="Path to top level release config file")
+    parser.add_argument("--task-filter", dest="task_filters", action='append',
+                        help="Task include filters per section name in the --config file to match with " \
+                        "the section name, e.g. 'offline', 'repository', ...")
+    args = parser.parse_args(sys.argv[1:])
+
+    assert os.path.isfile(args.config), "Not a valid file: {0}".format(args.config)
+    parse_config(args.config, args.task_filters)
---
+++ packaging-tools/qt6_installer/tests/test_release_task_reader.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#############################################################################
+##
+## Copyright (C) 2020 The Qt Company Ltd.
+## Contact: https://www.qt.io/licensing/
+##
+## This file is part of the release tools of the Qt Toolkit.
+##
+## $QT_BEGIN_LICENSE:GPL-EXCEPT$
+## Commercial License Usage
+## Licensees holding valid commercial Qt licenses may use this file in
+## accordance with the commercial license agreement provided with the
+## Software or, alternatively, in accordance with the terms contained in
+## a written agreement between you and The Qt Company. For licensing terms
+## and conditions see https://www.qt.io/terms-conditions. For further
+## information use the contact form at https://www.qt.io/contact-us.
+##
+## GNU General Public License Usage
+## Alternatively, this file may be used under the terms of the GNU
+## General Public License version 3 as published by the Free Software
+## Foundation with exceptions as appearing in the file LICENSE.GPL3-EXCEPT
+## included in the packaging of this file. Please review the following
+## information to ensure the GNU General Public License requirements will
+## be met: https://www.gnu.org/licenses/gpl-3.0.html.
+##
+## $QT_END_LICENSE$
+##
+#############################################################################
+
+import testhelpers
+import os
+import unittest
+from ddt import ddt
+from typing import List
+import release_task_reader
+import configparser
+
+
+@ddt
+class TestReleaseTaskReader(unittest.TestCase):
+
+    @testhelpers.asyncio_test_parallel_data(("linux,x64,common", ["linux", "x64", "common"]),
+                                            ("linux, x64,  common ", ["linux", "x64", "common"]),
+                                            ("linux; , x64  common ", ["linux", "x64", "common"]),
+                                            (": ,,; linux x64   common ", ["linux", "x64", "common"]))
+    async def test_get_filter_parts(self, task_filters: str, expected_result: List[str]) -> None:
+        self.assertEqual(release_task_reader.get_filter_parts(task_filters), expected_result)
+
+    @testhelpers.asyncio_test
+    async def test_release_task_reader(self) -> None:
+        sample_config = """
+            [task.repository.linux.x86_64]
+            config_file:  foobar-file-repository
+            substitutions: arg1, arg2, arg3
+            repo_components_to_update: *
+            repo_path: foo/bar/path
+            rta_key_list: key1, key2,key3   , key4
+
+            [task.offline.linux.x86_64.foobar]
+            config_file:  foobar-file-repository-2
+            substitutions: arg1, arg2, arg3
+            repo_components_to_update: *
+            repo_path: foo/bar/path2
+            rta_key_list: key1, key2,key3   , key4
+
+            [task.offline.linux.x86_64]
+            config_file:  foobar-file-offline
+            substitutions: arg11, arg21, arg31
+            rta_key_list: keyA, keyB
+
+            [task.online.linux.x86_64]
+            config_file:  foobar-file-online
+            substitutions: arg12, arg22, arg32
+            rta_key_list: key12, key22
+
+            [foo.online.linux.x86_64]
+            config_file:  foobar-file-online
+            substitutions: arg13, arg23, arg33
+            rta_key_list: key13, key23
+        """
+        config = configparser.ConfigParser()
+        config.read_string(sample_config)
+
+        # parse all tasks i.e. no filters
+        tasks = release_task_reader.parse_data(config, task_filters=[])
+        self.assertTrue(len(tasks) == 4, "Did not parse all tasks from sample config")
+
+        # parse only "repository" tasks
+        tasks = release_task_reader.parse_data(config, task_filters=["repository"])
+        self.assertTrue(len(tasks) == 1)
+        self.assertEqual(tasks[0].is_repository_task(), True)
+        self.assertEqual(tasks[0].is_offline_installer_task(), False)
+        self.assertEqual(tasks[0].is_online_installer_task(), False)
+        self.assertEqual(tasks[0].get_config_file(), "foobar-file-repository")
+        self.assertEqual(tasks[0].get_substitutions(), "arg1, arg2, arg3")
+        self.assertEqual(tasks[0].get_installer_string_replacement_list(), ["arg1", "arg2", "arg3"])
+        self.assertEqual(tasks[0].get_repo_components_to_update(), "*")
+        self.assertEqual(tasks[0].get_repo_path(), "foo/bar/path")
+        self.assertEqual(sorted(tasks[0].get_rta_key_list()), sorted(["key1", "key2", "key3", "key4"]))
+
+        # parse only "offline" tasks with multiple filters
+        tasks = release_task_reader.parse_data(config, task_filters=["offline,linux,x86_64"])
+        self.assertTrue(len(tasks) == 2)
+        tasks = release_task_reader.parse_data(config, task_filters=["offline,linux,x86_64,foobar"])
+        self.assertTrue(len(tasks) == 1)
+
+        # parse "offline" tasks with multiple filters and "online" tasks
+        tasks = release_task_reader.parse_data(config, task_filters=["offline,linux,x86_64", "online,linux,x86_64"])
+        self.assertTrue(len(tasks) == 3)
+
+    @testhelpers.asyncio_test
+    async def test_release_task_reader_invalid_config(self) -> None:
+        sample_config = """
+            [task.repository]
+            config_file:  foobar-file-repository
+            substitutions: arg1, arg2, arg3
+            repo_components_to_update: *
+            repo_path: foo/bar/path
+            rta_key_list: key1, key2
+        """
+        config = configparser.ConfigParser()
+        config.read_string(sample_config)
+        with self.assertRaises(release_task_reader.ReleaseTaskError):
+            release_task_reader.parse_data(config, task_filters=[])
+
+
+if __name__ == '__main__':
+    unittest.main()
