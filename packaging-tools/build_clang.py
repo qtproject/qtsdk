bef84403d7c1166d920de920334f0075bbeaf7c7
Cristian Adam <cristian.adam@gmail.com>
2020-06-05 16:19:00 +0200

    QtCreator_libclang: Use CMAKE_PREFIX_PATH instead of LLVM_ROOT
    This way find_package(Clang) in Clazy will find our built package,
    and not shomething else.
    
    CLANG_LIBRARY_IMPORT is also set to libclang. I don't understand
    why clazy requires a variable specifying the import library for
    MSVC.
    
    Change-Id: I2e3fdeac2d121ba3d673291a8d60f48603841aad
    Reviewed-by: Cristian Adam <cristian.adam@qt.io>

--- packaging-tools/build_clang.py
+++ packaging-tools/build_clang.py
@@ -331,7 +331,8 @@ def build_clazy(toolchain, src_path, build_path, install_path, bitness=64, envir
                  '-G', cmake_generator(toolchain),
                  '-DCMAKE_INSTALL_PREFIX=' + install_path,
                  '-DCMAKE_BUILD_TYPE=Release',
-                 '-DLLVM_ROOT=' + install_path]
+                 '-DCMAKE_PREFIX_PATH=' + install_path,
+                 '-DCLANG_LIBRARY_IMPORT=libclang']
     cmake_cmd.extend(bitness_flags(bitness))
     cmake_cmd.append(src_path)
     bldinstallercommon.do_execute_sub_process(cmake_cmd, build_path, extra_env=environment)
