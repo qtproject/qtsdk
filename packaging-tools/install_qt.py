7cab37ff262700cec7b3c163c1267dd180cb6342
Eike Ziller <eike.ziller@qt.io>
2021-04-14 13:07:28 +0200

    install-qt: Add simpler option for specifying modules
    Instead of providing a list of full URLs, optionally allow specifying
    a URL prefix, URL postfix, and a list of module names.
    
    Change-Id: I744d4aed56ededf204b2682c5fcc08d0b95f8de6
    Reviewed-by: Antti Kokko <antti.kokko@qt.io>

--- packaging-tools/install_qt.py
+++ packaging-tools/install_qt.py
@@ -40,8 +40,15 @@ def get_arguments():
     parser = argparse.ArgumentParser(description='Build Qt Creator')
     parser.add_argument('--qt-path', help='path to Qt', required=True)
     parser.add_argument('--qt-module', help='Qt module package url (.7z) needed for building',
-                        action='append', dest='qt_modules', required=True)
+                        action='append', dest='qt_modules')
     parser.add_argument('--temp-path', help='temporary path for downloads', required=True)
+
+    parser.add_argument('--base-url', help='Base URL for given module_name(s)')
+    parser.add_argument('module_name', help='Name of Qt module to install, based on --base-url and --base-url-postfix',
+                        nargs='*')
+    parser.add_argument('--base-url-postfix',
+                        help='Postfix to add to URLs constructed from --base-url and given module_name(s)',
+                        default='')
 
     # Linux
     parser.add_argument('--icu7z', help='a file or url where to get ICU libs as 7z')
@@ -54,6 +61,16 @@ def get_arguments():
     args = parser.parse_args()
     args.qt_path = os.path.abspath(args.qt_path)
     args.temp_path = os.path.abspath(args.temp_path)
+
+    if not args.qt_modules or args.base_url or args.module_name:
+        if not args.base_url or not args.module_name:
+            print('either --qt-module or --base-url and module_name(s) are required')
+            exit(1)
+
+    args.qt_modules = args.qt_modules if args.qt_modules else [] # ensure list
+    args.qt_modules += [args.base_url + '/' + module + '/' + module + args.base_url_postfix
+                        for module in args.module_name]
+
     return args
 
 def install_qt(args):
