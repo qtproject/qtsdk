5cc41473af2e2649c7bed65f5673e5a1db242c35
Samuli Piippo <samuli.piippo@qt.io>
2021-04-19 15:58:18 +0300

    Allow archive_name without repackaging
    Download the archive using the archive_name when the package
    doesn't need to repackaged.
    
    Change-Id: I90612c20236abb5ed11e474193c5c9bc66951aaf
    Reviewed-by: Iikka Eklund <iikka.eklund@qt.io>

--- packaging-tools/create_installer.py
+++ packaging-tools/create_installer.py
@@ -720,10 +720,9 @@ def get_component_data(sdk_component, archive, install_dir, data_dir_dest, compr
        and not archive.archive_action \
        and not archive.rpath_target \
        and sdk_component.target_install_base == '/' \
-       and not archive.target_install_dir \
-       and package_raw_name == archive.archive_name:
+       and not archive.target_install_dir:
         print '     No repackaging actions required for the package, just download it directly to data directory'
-        downloadedArchive = os.path.normpath(data_dir_dest + os.sep + package_raw_name)
+        downloadedArchive = os.path.normpath(data_dir_dest + os.sep + archive.archive_name)
         # start download
         bld_utils.download(archive.archive_uri, downloadedArchive)
         return
