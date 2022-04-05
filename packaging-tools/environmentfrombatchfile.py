f362c96d105e2c541015dcd2b610776b611f1928
Patrik Teivonen <patrik.teivonen@qt.io>
2022-04-05 10:13:52 +0300

    environmentfrombatchfile.py: Python3 port, check for duplicate path
    Update license. Remove unneeded print import in python3.6 as this file
    is used by other already ported scripts. Add sanity check for known
    duplicate path key in environment dictionary to avoid possible issues.
    
    Change-Id: Ia8b9f50d1b8f3b094ef13f6f69571fd36bb74070
    Reviewed-by: Iikka Eklund <iikka.eklund@qt.io>

--- packaging-tools/environmentfrombatchfile.py
+++ packaging-tools/environmentfrombatchfile.py
@@ -1,47 +1,33 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 #############################################################################
 ##
-## Copyright (C) 2013 Digia Plc and/or its subsidiary(-ies).
-## Contact: http://www.qt-project.org/legal
+## Copyright (C) 2022 The Qt Company Ltd.
+## Contact: https://www.qt.io/licensing/
 ##
 ## This file is part of the release tools of the Qt Toolkit.
 ##
-## $QT_BEGIN_LICENSE:LGPL$
+## $QT_BEGIN_LICENSE:GPL-EXCEPT$
 ## Commercial License Usage
 ## Licensees holding valid commercial Qt licenses may use this file in
 ## accordance with the commercial license agreement provided with the
 ## Software or, alternatively, in accordance with the terms contained in
-## a written agreement between you and Digia.  For licensing terms and
-## conditions see http://qt.digia.com/licensing.  For further information
-## use the contact form at http://qt.digia.com/contact-us.
-##
-## GNU Lesser General Public License Usage
-## Alternatively, this file may be used under the terms of the GNU Lesser
-## General Public License version 2.1 as published by the Free Software
-## Foundation and appearing in the file LICENSE.LGPL included in the
-## packaging of this file.  Please review the following information to
-## ensure the GNU Lesser General Public License version 2.1 requirements
-## will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
-##
-## In addition, as a special exception, Digia gives you certain additional
-## rights.  These rights are described in the Digia Qt LGPL Exception
-## version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
+## a written agreement between you and The Qt Company. For licensing terms
+## and conditions see https://www.qt.io/terms-conditions. For further
+## information use the contact form at https://www.qt.io/contact-us.
 ##
 ## GNU General Public License Usage
 ## Alternatively, this file may be used under the terms of the GNU
-## General Public License version 3.0 as published by the Free Software
-## Foundation and appearing in the file LICENSE.GPL included in the
-## packaging of this file.  Please review the following information to
-## ensure the GNU General Public License version 3.0 requirements will be
-## met: http://www.gnu.org/copyleft/gpl.html.
-##
+## General Public License version 3 as published by the Free Software
+## Foundation with exceptions as appearing in the file LICENSE.GPL3-EXCEPT
+## included in the packaging of this file. Please review the following
+## information to ensure the GNU General Public License requirements will
+## be met: https://www.gnu.org/licenses/gpl-3.0.html.
 ##
 ## $QT_END_LICENSE$
 ##
 #############################################################################
-
-# import the print function which is used in python 3.x
-from __future__ import print_function
 
 import itertools
 import subprocess
@@ -63,6 +49,18 @@ def consume(iterator):
             next(iterator)
     except StopIteration:
         pass
+
+
+class PackagingError(Exception):
+    pass
+
+
+def sanity_check_env(env_cmd, env_dict):
+    # throw error if 'Path' exists in environment dictionary
+    if "Path" in env_dict:
+        raise PackagingError("Configuration error, possible duplicate path 'Path' found in {0}"
+                        .format(env_cmd))
+
 
 def get(env_cmd, initial = None, arguments = None):
     """
@@ -101,4 +99,6 @@ def get(env_cmd, initial = None, arguments = None):
     result = dict(valid_pairs)
     # let the process finish
     proc.communicate()
+    # check resulting env for duplicate path
+    sanity_check_env(env_cmd, result)
     return result
