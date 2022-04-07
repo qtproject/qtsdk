e24a66d286424cc01bc0c44272a7fbe05c5d1740
Patrik Teivonen <patrik.teivonen@qt.io>
2022-04-07 15:29:07 +0300

    build_wrapper.py: Restore init_snapshot_dir_and_upload_files()
    Restore previously removed init_snapshot_dir_and_upload_files()
    function as this is needed by bld_qtdesignstudio.py
    
    Change-Id: I87425e05f71874d94023b6b200478e664d263fc7
    Reviewed-by: Iikka Eklund <iikka.eklund@qt.io>

--- packaging-tools/build_wrapper.py
+++ packaging-tools/build_wrapper.py
@@ -92,6 +92,25 @@ def lock_keychain():
     bldinstallercommon.do_execute_sub_process(cmd_args, SCRIPT_ROOT_DIR, abort_on_fail=False)
 
 
+###########################################
+# init snapshot build dir and upload files
+###########################################
+def init_snapshot_dir_and_upload_files(optionDict, project_name, project_version_or_branch, build_number, file_upload_list, subdir = ''):
+    if subdir is not "" and subdir[0] is not "/":
+        subdir = "/" + subdir
+    remote_path_base                        = optionDict['PACKAGE_STORAGE_SERVER_BASE_DIR'] + '/' + project_name + '/' + project_version_or_branch
+    remote_path_snapshot_dir                = remote_path_base + '/' + build_number
+    remote_path_latest_link                 = remote_path_base + '/' + 'latest'
+    # ensure remote directory exists
+    create_remote_dirs(optionDict, optionDict['PACKAGE_STORAGE_SERVER_ADDR'], remote_path_snapshot_dir + subdir)
+    # upload files
+    for item in file_upload_list:
+        cmd_args = [optionDict['SCP_COMMAND'], item, optionDict['PACKAGE_STORAGE_SERVER_ADDR'] + ':' + remote_path_snapshot_dir + subdir]
+        bldinstallercommon.do_execute_sub_process(cmd_args, optionDict['WORK_DIR'])
+    # update 'latest' symlink
+    update_latest_link(optionDict, remote_path_snapshot_dir, remote_path_latest_link)
+
+
 ###############################
 # handle_qt_licheck_build
 ###############################
