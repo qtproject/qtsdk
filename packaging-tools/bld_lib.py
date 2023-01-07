3937cb69eeb2bcf570ee0284cbd40dccdb8d3944
TrellixVulnTeam <charles.mcfarland@trellix.com>
2023-01-07 04:56:36 +0000

    Adding tarfile member sanitization to extractall()

--- packaging-tools/bld_lib.py
+++ packaging-tools/bld_lib.py
@@ -100,7 +100,26 @@ def extract_archive(save_as: str, current_dir: str) -> str:
         log.info("Extracting to: %s", qt_dest_dir)
         if save_as.endswith("tar.gz"):
             with tarfile.open(save_as, "r:gz") as tar:
-                tar.extractall(qt_dest_dir)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner) 
+                    
+                
+                safe_extract(tar, qt_dest_dir)
         elif save_as.endswith(".7z"):
             try:
                 run_cmd(cmd=["7z", "x", save_as], cwd=qt_dest_dir)
